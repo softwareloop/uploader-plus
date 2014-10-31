407d9e18bdba81b202507ac5a46e0f1a6ee7a186
Paolo Predonzani <paolo.predonzani@gmail.com>
2014-10-31 12:51:44 +0100

    close #36: Send metadata

--- surf/src/main/amp/web/components/uploader-plus/js/flash-upload-plus.js
+++ surf/src/main/amp/web/components/uploader-plus/js/flash-upload-plus.js
@@ -166,29 +166,40 @@ YAHOO.extend(SoftwareLoop.FlashUploadPlus, Alfresco.FlashUpload, {
         );
     },
 
+    processMetadata: function () {
+        var contentType = this.contentTypeSelectNode.value;
+        var record = this.records[this.currentRecordIndex];
+        var data = record.getData();
+        var firstTdEl = this.widgets.dataTable.getFirstTdEl(record);
+        var contentTypeEl = Dom.getElementsByClassName(
+            "fileupload-contentType-input", "input", firstTdEl);
+        if (contentTypeEl && contentTypeEl.length === 1) {
+            contentTypeEl[0].value = contentType;
+        } else {
+            console.log("contentTypeEl", contentTypeEl);
+        }
+        var secondTdEl = this.widgets.dataTable.getNextTdEl(firstTdEl);
+        var typeInfoEl = Dom.getElementsByClassName(
+            "fileupload-typeInfo-span", "span", secondTdEl);
+        if (typeInfoEl && typeInfoEl.length === 1) {
+            typeInfoEl[0].innerHTML =
+                Alfresco.util.encodeHTML("Content type: " + contentType);
+        } else {
+            console.log("typeInfoEl", typeInfoEl);
+        }
+
+        var formRuntime = this.formUi.formsRuntime;
+        var form = Dom.get(formRuntime.formId);
+        var formData = formRuntime._buildAjaxForSubmit(form);
+        this.fileStore[data.id].formData = formData;
+        console.log("formData", formData, this);
+    },
+
     onMetadataSubmit: function () {
-        console.debug("onMetadataSubmit");
+        console.debug("onMetadataSubmit", this.formUi);
         this.formUi.formsRuntime._setAllFieldsAsVisited();
         if (this.formUi.formsRuntime.validate()) {
-            var contentType = this.contentTypeSelectNode.value;
-            var record = this.records[this.currentRecordIndex];
-            var firstTdEl = this.widgets.dataTable.getFirstTdEl(record);
-            var contentTypeEl = Dom.getElementsByClassName(
-                "fileupload-contentType-input", "input", firstTdEl);
-            if (contentTypeEl && contentTypeEl.length === 1) {
-                contentTypeEl[0].value = contentType;
-            } else {
-                console.log("contentTypeEl", contentTypeEl);
-            }
-            var secondTdEl = this.widgets.dataTable.getNextTdEl(firstTdEl);
-            var typeInfoEl = Dom.getElementsByClassName(
-                "fileupload-typeInfo-span", "span", secondTdEl);
-            if (typeInfoEl && typeInfoEl.length === 1) {
-                typeInfoEl[0].innerHTML =
-                    Alfresco.util.encodeHTML("Content type: " + contentType);
-            } else {
-                console.log("typeInfoEl", typeInfoEl);
-            }
+            this.processMetadata();
             this.currentRecordIndex++;
             this.showMetadataDialog();
         } else {
@@ -263,6 +274,97 @@ YAHOO.extend(SoftwareLoop.FlashUploadPlus, Alfresco.FlashUpload, {
     _resetGUI: function () {
         this.showMainDialog();
         SoftwareLoop.FlashUploadPlus.superclass._resetGUI.apply(this, arguments);
-    }
+    },
+
+    _uploadFromQueue: function FlashUpload__uploadFromQueue(noOfUploadsToStart) {
+        // generate upload POST url
+        var url;
+        if (this.showConfig.uploadURL === null) {
+            url = Alfresco.constants.PROXY_URI + "api/upload";
+        }
+        else {
+            url = Alfresco.constants.PROXY_URI + this.showConfig.uploadURL;
+        }
+
+        // Flash does not correctly bind to the session cookies during POST
+        // so we manually patch the jsessionid directly onto the URL instead
+        // also include the CSRF token to pass the CSRF token filter
+        url += ";jsessionid=" + YAHOO.util.Cookie.get("JSESSIONID") + "?lang=" + Alfresco.constants.JS_LOCALE;
+
+        // Pass the CSRF token if the CSRF token filter is enabled
+        if (Alfresco.util.CSRFPolicy.isFilterEnabled()) {
+            url += "&" + Alfresco.util.CSRFPolicy.getParameter() + "=" + encodeURIComponent(Alfresco.util.CSRFPolicy.getToken());
+        }
+
+        // Find files to upload
+        var startedUploads = 0,
+            length = this.widgets.dataTable.getRecordSet().getLength(),
+            record, flashId, fileInfo, attributes;
+
+        for (var i = 0; i < length && startedUploads < noOfUploadsToStart; i++) {
+            record = this.widgets.dataTable.getRecordSet().getRecord(i);
+            flashId = record.getData("id");
+            fileInfo = this.fileStore[flashId];
+            if (fileInfo.state === this.STATE_BROWSING) {
+                // Upload has NOT been started for this file, start it now
+                fileInfo.state = this.STATE_UPLOADING;
+
+                attributes =
+                {
+                    username: this.showConfig.username
+                };
+
+                // Site or Non-site (Repository) mode
+                if (this.showConfig.siteId !== null) {
+                    attributes.siteId = this.showConfig.siteId;
+                    attributes.containerId = this.showConfig.containerId;
+                }
+                else if (this.showConfig.destination !== null) {
+                    attributes.destination = this.showConfig.destination
+                }
+
+                if (this.showConfig.mode === this.MODE_SINGLE_UPDATE) {
+                    attributes.updateNodeRef = this.showConfig.updateNodeRef;
+                    attributes.majorVersion = !this.minorVersion.checked;
+                    attributes.description = this.description.value;
+                }
+                else {
+                    if (this.showConfig.uploadDirectory !== null) {
+                        attributes.uploadDirectory = this.showConfig.uploadDirectory;
+                    }
+                    if (fileInfo.contentType) {
+                        if (fileInfo.contentType.tagName.toLowerCase() == "select") {
+                            attributes.contentType = fileInfo.contentType.options[fileInfo.contentType.selectedIndex].value;
+                        }
+                        else {
+                            attributes.contentType = fileInfo.contentType.value;
+                        }
+                    }
+                    attributes.overwrite = this.showConfig.overwrite;
+                    if (this.showConfig.thumbnails) {
+                        attributes.thumbnails = this.showConfig.thumbnails;
+                    }
+                }
+
+                // BEGIN: uploader-plus customisations
+                console.log("fileInfo", fileInfo);
+                if (fileInfo.formData) {
+                    for (var current in fileInfo.formData) {
+                        if (fileInfo.formData.hasOwnProperty(current) &&
+                            current.indexOf("prop_") === 0) {
+                            attributes[current] = fileInfo.formData[current];
+                        }
+                    }
+                }
+                console.log("Attributes:", attributes);
+
+                // END: uploader-plus customisations
+
+                this.uploader.upload(flashId, url, "POST", attributes, "filedata");
+                startedUploads++;
+            }
+        }
+    },
+
 });
 
