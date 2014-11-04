6b2845134dc2449c2235a8058154e20b168af68d
Paolo Predonzani <paolo.predonzani@gmail.com>
2014-11-04 17:45:09 +0100

    close #22 Plain HTML uploader

---
+++ surf/src/main/amp/web/components/uploader-plus/js/html-upload-plus.js
@@ -0,0 +1,137 @@
+(function () {
+    var oldConstructor = Alfresco.HtmlUpload;
+
+    Alfresco.HtmlUpload = function (htmlId) {
+        var that = new oldConstructor(htmlId);
+        YAHOO.lang.augmentObject(that, SoftwareLoop.UploaderPlusMixin);
+        YAHOO.lang.augmentObject(that, {
+            //**************************************************************************
+            // Initialisation at show
+            //**************************************************************************
+
+            show: function (config) {
+                console.debug("show", this);
+                this.showMainDialog();
+                Alfresco.HtmlUpload.prototype.show.call(this, config);
+
+                // test if types is undefined
+                // types == null means any content can be uploaded without prompting for metadata
+                if (typeof(this.types) === "undefined") {
+                    this.loadTypes(SoftwareLoop.hitch(this, function () {
+                        this.populateSelect();
+                        if (this.types) {
+                            this.fixButtons();
+                        }
+                    }));
+                }
+            },
+
+            fixButtons: function () {
+                console.debug("fixButtons", this.widgets.form);
+                this.widgets.uploadButton.set("label", "Next");
+                this.widgets.uploadButton.removeListener("click");
+                this.widgets.uploadButton.on(
+                    "click", function (event) {
+                        console.debug(event);
+                        this.widgets.form._setAllFieldsAsVisited();
+                        if (this.widgets.form.validate()) {
+                            this.showMetadataDialog();
+                        }
+                    }, this, this);
+            },
+
+
+            //**************************************************************************
+            // Metadata dialog management
+            //**************************************************************************
+
+            showMetadataDialog: function () {
+                console.debug("showMetadataDialog");
+                YAHOO.util.Dom.addClass(this.id + "-main-dialog", "fake-hidden");
+                YAHOO.util.Dom.removeClass(this.id + "-metadata-dialog", "hidden");
+
+                var filename = this.widgets.filedata.files[0].name;
+
+                this.currentRecordIndex = 0;
+                this.records = [];
+                this.records.push({
+                    getData: function () {
+                        return {
+                            name: filename
+                        }
+                    }
+                });
+
+                SoftwareLoop.fireEvent(this.contentTypeSelectNode, "change");
+            },
+
+            showMainDialog: function () {
+                console.debug("showMainDialog");
+                delete this.records;
+                delete this.currentRecordIndex;
+
+                YAHOO.util.Dom.removeClass(this.id + "-main-dialog", "fake-hidden");
+                YAHOO.util.Dom.addClass(this.id + "-metadata-dialog", "hidden");
+            },
+
+            //**************************************************************************
+            // Form button handling
+            //**************************************************************************
+
+            onMetadataSubmit: function (event) {
+                console.debug("onMetadataSubmit", this.formUi);
+                this.formUi.formsRuntime._setAllFieldsAsVisited();
+                if (this.formUi.formsRuntime.validate()) {
+                    this.processMetadata();
+                    this.widgets.form._submitInvoked.call(this.widgets.form, event);
+                } else {
+                    Alfresco.util.PopupManager.displayMessage({
+                        text: this.msg("validation.errors.correct.before.proceeding")
+                    });
+                }
+            },
+
+            processMetadata: function () {
+                var contentTypeInputNode =
+                    YAHOO.util.Dom.get(
+                            this.id + "-contentTypeInput");
+                if (contentTypeInputNode) {
+                    contentTypeInputNode.value =
+                        this.contentTypeSelectNode.value;
+                }
+
+                var formRuntime = this.formUi.formsRuntime;
+                var form = Dom.get(formRuntime.formId);
+                var propertyData = formRuntime._buildAjaxForSubmit(form);
+
+                var submitForm =
+                    YAHOO.util.Dom.get(this.widgets.form.formId);
+                for (var current in propertyData) {
+                    if (propertyData.hasOwnProperty(current) &&
+                        current.indexOf("prop_") === 0) {
+                        var input = document.createElement("input");
+                        input.setAttribute("type", "hidden");
+                        input.setAttribute("name", current);
+                        input.setAttribute("value", propertyData[current]);
+                        submitForm.appendChild(input);
+                    }
+                }
+
+                console.log("propertyData", propertyData, submitForm);
+            },
+
+            onMetadataCancel: function () {
+                console.debug("onMetadataCancel");
+                this.showMainDialog();
+            }
+
+            //**************************************************************************
+            // Upload override
+            //**************************************************************************
+
+        }, true);
+        return that;
+    };
+    Alfresco.HtmlUpload.superclass = oldConstructor.superclass;
+    Alfresco.HtmlUpload.prototype = oldConstructor.prototype;
+})();
