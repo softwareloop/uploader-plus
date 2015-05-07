30e059253f807c8588f108ba819bb22ee69d56e5
Douglas C. R. Paes <douglasp@konsultex.com.br>
2015-05-07 15:56:14 -0300

    Fixed the import resource line format. It was causing error on the server:
    org.springframework.extensions.webscripts.WebScriptException: 04070005 Failed to load script 'classpath*:alfresco/site-webscripts/com/softwareloop/uploader-plus/uploader
    /dnd-upload.get.js': 04070004 Malformed 'import' line - must be first in file, no comments and strictly of the form:
    <import resource="...">
    
    and
    
    2015-05-07 14:31:51,374  ERROR [extensions.webscripts.AbstractRuntime] [http-bio-8080-exec-3] Exception from executeScript - redirecting to status template error: 04070003 Failed to load script 'classpath*:alfresco/site-webscripts/com/softwareloop/uploader-plus/uploader/flash-upload.get.js': 04070002 Malformed 'import' line - must be first in file, no comments and strictly of the form:^M
    <import resource="...">

--- surf/src/main/amp/config/alfresco/site-webscripts/com/softwareloop/uploader-plus/uploader/flash-upload.get.js
+++ surf/src/main/amp/config/alfresco/site-webscripts/com/softwareloop/uploader-plus/uploader/flash-upload.get.js
@@ -1,4 +1,4 @@
-<import resource="classpath:alfresco/site-webscripts/org/alfresco/components/upload/flash-upload.get.js" />
+<import resource="classpath:/alfresco/site-webscripts/org/alfresco/components/upload/flash-upload.get.js">
 // The above import allows us to reuse Alfresco original flash-upload component web script.
 // When end user overrides via web-extension directory, this won't pick up the override.
 // This is a drawback of Alfresco JS importing.
