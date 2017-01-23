85af142c6d66af802b56a80f144ac0f03cbdeb9a
yregaieg <younes.regaieg@xenit.eu>
2017-01-23 10:51:15 +0100

    Better view for large forms

--- surf/src/main/amp/web/components/uploader-plus/js/uploader-plus-mixin.js
+++ surf/src/main/amp/web/components/uploader-plus/js/uploader-plus-mixin.js
@@ -168,6 +168,10 @@
                 oldOnReady.apply(this.formUi, arguments);
                 this.formUiFixButtons();
             });
+            var formFieldsId = this.id + "-metadata-form-form-fields";
+            YAHOO.util.Dom.setStyle(formFieldsId, 'height', '550px');
+            YAHOO.util.Dom.setStyle(formFieldsId, 'overflow-x', 'auto');
+
     
             this.centerPanel();
     
